dbe3702120c8f663fa5d543525ecee01eeb6bbac
llievredemars <31421134+llievredemars@users.noreply.github.com>
2017-11-23 21:30:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&fi
 
 $(document).ready(function() {
     getQuote();
-    $('.trigger').click(function() {
-        getQuote();
-    })
+    $('.trigger').click(getQuote());
 });
 function getQuote() {
     $.getJSON(quoteUrl, createTweet);
